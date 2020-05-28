9bbda42cccd494db94080f1fd206b4f6e03c2d4d
wilsogonzalez@uan.edu.co <Gonzalez Tovar@192.168.0.10>
2020-05-28 09:29:12 -0500

    cambio test

--- ProyectoCalidad/src/com/view/LoginTest.java
+++ ProyectoCalidad/src/com/view/LoginTest.java
@@ -29,8 +29,6 @@ public class LoginTest {
 	@Test
 	public void testLogin() {
 	
-		
-
 			/**
 			 * Create the frame.
 			 */
@@ -60,11 +58,57 @@ public class LoginTest {
 							JOptionPane.showMessageDialog(null, "!!Bienvenido!!");
 							paginaPrincipal.setVisible(true);
 							dispose();
+							}
+						
 						}
-						else {
-							JOptionPane.showMessageDialog(null, "Usuario y/o Contraseña Incorrectos");	
+						catch (Exception login) {
+							System.out.println(e);
 						}
-						}
+						
+		
+					}
+					
+				});
+				
+									
+			}
+
+	protected void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Test
+	public void testLoginfallida() {
+	
+			/**
+			 * Create the frame.
+			 */
+				contentPane = new JPanel();
+				contentPane.setBorder(new EmptyBorder(3, 3, 3, 3));
+				void setContentPane(contentPane);
+				
+				JLabel lblNombre = new JLabel("Usuario:");
+				lblNombre.setFont(new Font("Tahoma", Font.PLAIN, 20));
+				
+			
+				
+				JLabel lblContrasea = new JLabel("Contrase\u00F1a:");
+				lblContrasea.setFont(new Font("Tahoma", Font.PLAIN, 20));
+				
+				
+				JButton btnEntrar = new JButton("Entrar");
+				btnEntrar.addActionListener(new ActionListener() {
+										
+					public void actionPerformed(ActionEvent e) {
+						try {
+						PreparedStatement ps = DataConnectionUser.con.prepareStatement("Select * from Usuario where Nombre='"+txt_usuario.getText()+"'and Contraseña='"+txt_contraseña.getText()+"'");
+						ResultSet rs = ps.executeQuery();
+						if(rs.next()){
+							JOptionPane.showMessageDialog(null, "Usuario y/o Contraseña Incorrectos");
+							paginaPrincipal.setVisible(true);
+							dispose();
+							}
+												}
 						catch (Exception login) {
 							System.out.println(e);
 						}
@@ -78,15 +122,8 @@ public class LoginTest {
 						
 			
 			}
-
-	protected void dispose() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	
-		
-		}
+			
+}
 	
 	
 
